61cd7bd16c366f5c1c41eb7116e5d0bdd1b8f6da
zuoxiupeng <zuoxiupeng@live.com>
2018-10-21 15:22:09 +0800

    实现可自定义日志输出对象，便于开发统一管理日志

--- logger.go
+++ logger.go
@@ -2,7 +2,7 @@
  * @Author: 时光弧线
  * @Date: 2017-12-30 11:55:33
  * @Last Modified by: 时光弧线
- * @Last Modified time: 2017-12-30 13:02:01
+ * @Last Modified time: 2018-10-21 14:41:00
  */
 package tcplibrary
 
@@ -13,20 +13,39 @@ import (
 
 /* tcp 内部日志打印 */
 
+// TCPLibraryLogger 日志接口，可以使用第三方日志库，符合接口即可
+type TCPLibraryLogger interface {
+	Infof(format string, a ...interface{})
+	Warnf(format string, a ...interface{})
+	Errorf(format string, a ...interface{})
+	Fatalf(format string, a ...interface{})
+}
+
+// tcplibrary 全局日志对象
+var globalLogger TCPLibraryLogger
+
+// SetGlobalLogger 设置日志对象
+func SetGlobalLogger(logger TCPLibraryLogger) {
+	if logger != nil {
+		globalLogger = logger
+	} else {
+		globalLogger.Errorf("设置tcplibrary日志对象不能是nil")
+	}
+}
+
+func init() {
+	defaultLogger := new(Logger)
+	defaultLogger.debug = true
+	globalLogger = defaultLogger
+}
+
 // Logger 日志记录
 type Logger struct {
 	debug bool
 }
 
-var globalLogger *Logger
-
-func init() {
-	globalLogger = new(Logger)
-	globalLogger.debug = true
-}
-
-// SetDebug 设置是否为debug模式
-func (l *Logger) SetDebug(debug bool) {
+// SetDefaultDebug 设置是否为debug模式 - 只对默认日志库生效
+func (l *Logger) SetDefaultDebug(debug bool) {
 	l.debug = debug
 }
 
